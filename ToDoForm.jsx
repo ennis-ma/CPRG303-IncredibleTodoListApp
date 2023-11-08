a48664726376df98f6bd5df819f4f6effd9baa3b
ennis-ma <ennis@hustle.hk>
2023-11-08 00:02:51 -0700

    lab3

--- ToDoForm.jsx
+++ ToDoForm.jsx
@@ -4,7 +4,7 @@ import { View, TextInput, Button, StyleSheet } from 'react-native';
 const ToDoForm = () => {
   return (
     <View style={styles.form}>
-      <TextInput style={styles.input} placeholder="Add a new task.." />
+      <TextInput style={styles.input} placeholder="Add a new task..." />
       <Button title="Add" />
     </View>
   );
